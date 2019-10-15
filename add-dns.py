aced1d35870f311684ede4e89f0bc95435b3ca4d
kumaran85 <mkumaran.anand@gmail.com>
2019-10-15 17:46:45 +0530

    Tried to add DNS using python. not working

---
+++ add-dns.py
@@ -0,0 +1,22 @@
+#!/bin/python_new
+#from __future__ import print_function
+#fr = open('/home/ku290295/python/resl.conf' , 'r')
+line_number = 2
+#read_cont=fr.read()
+#var = (int(read_cont.find('8.8.8.8')))
+#fr.close()
+#if (var == -1) :
+# print("Before changing")
+# fil = open('/home/ku290295/python/resl.conf' , 'r+')
+# rea=fil.readlines()
+# for lin in rea :
+#    print(line_number)
+#    fil.seek(0)
+#    rea.insert(line_number , "nameserver 8.8.8.8\nnameserver 64.6.64.6")
+#    lin.writelines(fil)
+#    print(fil)
+## fil.write("nameserver 8.8.8.8\nnameserver 64.6.64.6")
+# fil.close()
+# print("if completed")
+#else:
+# print(read_cont)
