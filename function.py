98fd6320d01b9349f20b9f86f3f0d61f270755be
kumaran85 <mkumaran.anand@gmail.com>
2019-10-15 17:55:54 +0530

    Function example in Python

---
+++ function.py
@@ -0,0 +1,9 @@
+#!/bin/python
+def hello(one):
+  print "hai This is values "
+  hai = one + 8
+  print hai
+one = 15
+hello(one)
+ohai={ 'name': 'ku290295', 'id': 25}
+print "Name of person" ,ohai['id']
