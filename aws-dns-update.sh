fae815f8dfcaee3006cd45362185d38986961508
kumaran85 <mkumaran.anand@gmail.com>
2019-11-21 18:10:22 +0530

    Just modified file path

--- aws-dns-update.sh
+++ aws-dns-update.sh
@@ -6,8 +6,8 @@ echo "Just for Test" >> /home/ec2-user/dns-executing
 echo $ips
 if [[ $ips != "64.6.64.6" ]];
  then
-  sed -i '3 i nameserver 64.6.64.6' resl.conf
-  sed -i '4 i nameserver 8.8.8.8' resl.conf
+  sed -i '3 i nameserver 64.6.64.6' /etc/reslov.conf
+  sed -i '4 i nameserver 8.8.8.8' /etc/reslov.conf
 else
    echo "DNS exists"
 fi
