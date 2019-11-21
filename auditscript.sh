646ce4c0134306c9f3f5df186d5ead46e21c9107
kumaran85 <mkumaran.anand@gmail.com>
2019-11-21 18:11:52 +0530

    Modified mkdir outside of for loop

--- auditscript.sh
+++ auditscript.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 ls -lhtr /var/log/audit/audit.log.* | awk ' {print $9}' > /logfilename
-#!/bin/bash
+mkdir audit_backup
 for nam in $(cat /logfilename)
 do
 cd /var/log/audit
 tar -cvzf $nam.tar.gz $nam
 rm -rf $nam
 echo "script doing @`date +%Y%m%d-%H-%M`" >>/access.log
-mkdir audit_backup
 mv *.gz audit_backup
 done
 exit
